08740678ec475c5057d312c71cec2ad26b5f0bdb
Ryabchikova Anastasiya <a.ryabchikova@rambler-co.ru>
2020-08-11 16:31:54 +0300

    Add router links to header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,23 +1,24 @@
 import './header.css';
 
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default class Header extends React.Component {
-    menuItems = [{id: 'people',label: 'People'}, {id: 'planets', label: 'Planets'}, {id: 'starships', label: 'Starships'}];
-    render() {
-
-        const menuLinks = this.menuItems.map(item => {
-            return (
-            <a className='nav-item' href={item.id} key={item.id}>{item.label}</a>
-            )
-        });
+const Header = () => {
+    const menuItems = [{id: 'people',label: 'People'}, {id: 'planets', label: 'Planets'}, {id: 'starships', label: 'Starships'}];
+    const menuLinks = menuItems.map(item => {
         return (
-            <header className='header'>
-                <span className='logo'>Star Wars DB</span>
-                <nav>
-                    {menuLinks}
-                </nav>
-            </header>
+        <Link className='nav-item' to={item.id} key={item.id}>{item.label}</Link>
         )
-    }
+    });
+    return (
+        <header className='header'>
+            <span className='logo'>Star Wars DB</span>
+            <nav>
+                {menuLinks}
+            </nav>
+        </header>
+    )
+    
 }
+
+export default Header;
