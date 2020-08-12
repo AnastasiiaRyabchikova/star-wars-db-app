0af855de88c0b9b8f07eee29c976d70a030a8bbc
Ryabchikova Anastasiya <a.ryabchikova@rambler-co.ru>
2020-08-12 15:17:58 +0300

    Fix redirect

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from '../header';
 import RandomPlanet from '../random-planet';
 import { PeoplePage, PlanetsPage, StarshipsPage, SecretPage, LoginPage } from '../pages';
 import ErrorBoundry from '../error-boundry';
 import { SwapiServiceProvider } from '../swapi-service-context'
-// import Row from '../row';
 import SwapiService from '../../services/swapi-service'
 
 
@@ -33,17 +32,18 @@ export default class App extends React.Component {
                             <Header />
                             <RandomPlanet/>
 
-                            <Route path="/" exact={true} render={ () => <h2>Welcome to Star DB</h2> }/>
+                            <Switch>
 
-                            <Route path="/people/:id?" render={  ({match}) => <PeoplePage itemId={match.params.id}/> } />
-
-                            <Route path="/planets/:id?" render={ ({match}) => <PlanetsPage itemId={match.params.id}/> } />
+                                <Route path="/" exact={true} render={ () => <h2>Welcome to Star DB</h2> }/>
+                                <Route path="/people/:id?" render={  ({match}) => <PeoplePage itemId={match.params.id}/> } />
+                                <Route path="/planets/:id?" render={ ({match}) => <PlanetsPage itemId={match.params.id}/> } />
+                                <Route path="/starships/:id?" render={ ({match}) => <StarshipsPage itemId={match.params.id} /> } />
+                                <Route path="/login" render={ () => <LoginPage isLoggedIn={isLoggedIn} onLogin={ this.onLogin }/> } />
+                                <Route path="/secret" render={ () => <SecretPage isLoggedIn={isLoggedIn} /> } />
+                                
+                                <Route render={ () => <div><h1 style={{textAlign: 'center'}}>404</h1><p style={{textAlign: 'center'}}>Page not found</p></div> } />
                             
-                            <Route path="/starships/:id?" render={ ({match}) => <StarshipsPage itemId={match.params.id} /> } />
-                            
-                            <Route path="/login" render={ () => <LoginPage isLoggedIn={isLoggedIn} onLogin={ this.onLogin }/> } />
-                            
-                            <Route path="/secret" render={ () => <SecretPage isLoggedIn={isLoggedIn} /> } />
+                            </Switch>
                         
                         </div>
                     </Router>
